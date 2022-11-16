f56cfe2beb6d422852a8d93d9e9681f7ec9aeb0d
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2022-11-16 12:35:35 +0000

    Add files via upload

---
+++ WorkingWithDatabases/CodeFirstEF/Migrations/20220606144753_FilmReviewsDB.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCodeFirstFilmReviewsDB.Migrations
+{
+    public partial class FilmReviewsDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    FilmId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Overview = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Revenue = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.FilmId);
+                });
+
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FilmId = table.Column<int>(type: "int", nullable: false),
+                    ReviewerId = table.Column<int>(type: "int", nullable: false),
+                    Commentary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Films_FilmId",
+                        column: x => x.FilmId,
+                        principalTable: "Films",
+                        principalColumn: "FilmId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_FilmId",
+                table: "Reviews",
+                column: "FilmId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
---
+++ WorkingWithDatabases/NoSQLDB/Program.cs
@@ -0,0 +1,117 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using NoSQLFilmReviews.Models;
+
+namespace NoSQLFilmReviews
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://demo-user:sTqTKzzKL5gm3YQh@cluster0.hgliefa.mongodb.net/?retryWrites=true&w=majority");
+
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase("test");
+
+            // Get list of databases within the cluster connected to
+            var databases = client.ListDatabases().ToList();
+            foreach (var db in databases)
+            {
+                Console.WriteLine(db["name"]);
+            }
+
+            Film film1 = new Film { Id = 1, Title = "Titanic", Overview = "Blab, blah ,blah", ReleaseDate = new DateTime(1997, 11, 18), Revenue = 1845034188, Reviews = new List<Review>() };
+            Film film2 = new Film { Id = 2, Title = "ET", Overview = "An extra terrestrial comes to earth, gets sick, phones home and is rescued", ReleaseDate = new DateTime(2004, 03, 19), Revenue = 72258126, Reviews = new List<Review>() };
+
+            database = client.GetDatabase("FilmReviews");
+            var reviews = database.GetCollection<Review>("Reviews");
+            var films = database.GetCollection<Film>("Films");
+            reviews.DeleteMany(r => true);
+            films.DeleteMany(f => true);
+
+            Review review = new Review
+            {
+                Id = 1000,
+                FilmId = film1.Id,
+                Commentary = "Truly awful",
+                Rating = 2,
+            };
+
+            reviews.InsertOne(review);
+            film1.Reviews.Add(review);
+
+            review = new Review
+            {
+                Id = 1001,
+                FilmId = film1.Id,
+                Commentary = "Gives you a sinking feeling in your stomach",
+                Rating = 8,
+            };
+
+            reviews.InsertOne(review);
+            film1.Reviews.Add(review);
+
+            review = new Review
+            {
+                Id = 1002,
+                FilmId = film1.Id,
+                Commentary = "Chilly",
+                Rating = 6,
+            };
+            reviews.InsertOne(review);
+            film1.Reviews.Add(review);
+
+            review = new Review
+            {
+                Id = 1003,
+                FilmId = film2.Id,
+                Rating = 9,
+            };
+            reviews.InsertOne(review);
+            film2.Reviews.Add(review);
+
+            films.InsertOne(film1);
+            films.InsertOne(film2);
+
+            // Get all films with at least 2 reviews
+            List<Film> filmsWithReviews = films.Find(f => f.Reviews.Count >= 2).ToList();
+            foreach (Film film in filmsWithReviews)
+            {
+                System.Console.WriteLine(film.Title);
+                foreach (Review rev in film.Reviews)
+                {
+                    Console.WriteLine($"{rev.Commentary} Rating ({rev.Rating})");
+                }
+            }
+
+            // Delete an item from a collection
+            films.FindOneAndDelete(f => f.Title == "ET");
+
+            films.InsertOne(new Film
+            {
+                Id = 3,
+                Title = "Up",
+                Overview = "The ultimate house move",
+                ReleaseDate = new DateTime(2012, 04, 21)
+            });
+
+
+            films.InsertOne(new Film
+            {
+                Id = 4,
+                Title = "Where Eagles Dare",
+                Overview = "A Wonkymotion remake of the original. Truly awe inspiring.",
+                ReleaseDate = new DateTime(2022, 04, 21),
+                HomepageURL = "https://www.youtube.com/watch?v=ENrgZ4KAnNw"
+            });
+
+            // Retrieve films and show revenue, even though some films don't have them
+            foreach (Film film in films.Find(d => true).ToList())
+            {
+                Console.WriteLine($"{film.Title} has a revenue of ${(film.Revenue != null ? film.Revenue : 0)}, Hompage: {(film.HomepageURL != null ? film.HomepageURL : "No Hompage")}");
+
+            }
+        }
+    }
+}
